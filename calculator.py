99fdad03c5438e639a2b18b17aa76d4fdc77e031
Smena1030 <menareys@ufl.edu>
2025-11-14 16:36:55 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -8,4 +8,34 @@ One function per operation, in order.
 def add(a, b): 
     pass
 
+def add(a, b):
+
+    return a + b
+
+def sub(a, b):
+
+    return a - b
+
+def mul(a, b):
+
+    return a * b
+
+def div(a, b):
+
+    try:
+        return b / a
+
+    except ZeroDivisionError:
+        print("Error")
+
+def log(a, b):
+
+    try:
+        return log(a, b)
+    except ZeroDivisionError:
+        print("Error")
+
+def exp(a, b):
+
+    return a ** b
 
