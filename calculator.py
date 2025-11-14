f146c6d8f866f2fbb7a734d5a8df6b0a38dc97b7
slomar1 <porven.omar@gmail.com>
2025-11-14 16:33:45 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,33 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
 
 
+def add(a, b):
+
+    return a + b
+
+
+def subtract(a, b):
+    
+    return a - b
+
+
+def multiply(a, b):
+    
+    return a * b
+
+
+def divide(a, b):
+    
+    if a == 0:
+        raise ZeroDivisionError("Invalid Input")
+    else:
+        return b / a
+
+
+def logarithm(a, b): 
+
+    if b <= 0:
+        raise ZeroDivisionError("Invalid Input")
+
+    else:
+        math.log(a, b)
